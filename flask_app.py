6a0de677f61a74e164c701701a3377bc1e0413da
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-01 18:34:42 -0700

    test

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Again from SurePortfolio!'
+    return 'Hello from SurePortfolio!'
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
