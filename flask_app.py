b92253583dbc4d7549cb8d287805db38937e070b
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-01 18:51:47 -0700

    Added GET method

--- flask_app.py
+++ flask_app.py
@@ -10,12 +10,16 @@ app = Flask(__name__)
 def hello_world():
     return 'TEST 123 Latest Message from SurePortfolio!'
 
-@app.route('/update_server', methods=['POST'])
+@app.route('/update_server', methods=['POST','GET'])
 def webhook():
     if request.method == 'POST':
         repo = git.Repo('mysite')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
+    
+    elif request.method == 'GET':
+        return "GET"
+    
     else:
         return 'Wrong event type', 400
