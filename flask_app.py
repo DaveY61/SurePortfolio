1babcd59108c7f985ca55a0a3ce581ac14de495e
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-12 10:20:10 -0700

    updated

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,25 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask,request
 import git
+from check_signature import is_valid_signature
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+w_secret = os.getenv('GitHub_SECRET')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Message from SurePortfolio!'
+    return 'NEW Message from SurePortfolio!'
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
+    x_hub_signature = request.headers.get("X-Hub-Signature")
+    if not is_valid_signature(x_hub_signature, request.data, w_secret):
+        return 'Invalid Signature', 400
+
     if request.method == 'POST':
         repo = git.Repo('mysite')
         origin = repo.remotes.origin
@@ -20,3 +28,6 @@ def webhook():
     
     else:
         return 'Wrong event type', 400
+    
+if __name__ == '__main__':
+    app.run(debug=True)
