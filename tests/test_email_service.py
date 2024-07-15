b535b5ddd6c74d9d50ff9b6680a2dbb8022e9b88
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-14 17:22:12 -0700

    Added path for email test

--- tests/test_email_service.py
+++ tests/test_email_service.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the parent directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from services.email_service import send_email,check_and_resend_failed_emails
 
 def test_email_service():
